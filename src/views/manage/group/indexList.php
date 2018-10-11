e2a772ba430c0c975850627cf2fae9cc630d08c8
SAM2O2O <an.guoyue254@gmail.com>
2018-10-11 21:54:31 +0800

    update site manage userList

--- src/views/manage/group/indexList.php
+++ src/views/manage/group/indexList.php
@@ -7,30 +7,100 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <link rel="stylesheet" href="../../public/manage/config.css"/>
+    <link rel="stylesheet" href="../../public/manage/search.css"/>
+
+    <style>
+        .item-row-title {
+            /*width: 100%;*/
+            height: 20px;
+            font-size: 14px;
+            font-family: PingFangSC-Medium;
+            font-weight: 500;
+            color: rgba(153, 153, 153, 1);
+            line-height: 20px;
+            margin: 17px 0px 7px 10px;
+        }
+    </style>
 </head>
 
 <body>
 
 <div class="wrapper" id="wrapper">
 
+    <div class="item-search">
+        <img class="search-img" width="19px" height="19px" src="../../public/img/manage/search.png">
+
+        <?php if ($lang == "1") { ?>
+            <input type="text" id="search-group" class="search-input" placeholder="通过群名称搜索群组">
+        <?php } else { ?>
+            <input type="text" id="search-group" class="search-input" placeholder="search groups by name">
+        <?php } ?>
+    </div>
+
+    <div class="layout-all-row" id="search-content" style="display: none">
+        <div class="list-item-center">
+
+            <div id="search-title" class="item-row-title">
+                <?php if ($lang == "1") { ?>
+                    群组搜索结果
+                <?php } else { ?>
+                    Search Groups
+                <?php } ?>
+
+            </div>
+
+            <div class="item-row group-list">
+                <div class="item-body" onclick="showGroupProfile('<?php echo($profile["groupId"]) ?>');">
+                    <div class="item-body-display">
+                        <div class="item-body-desc">
+                            <?php echo $profile["name"]; ?>
+                        </div>
+
+                        <div class="item-body-tail">
+                            <div class="item-body-value">
+                                <img class="more-img"
+                                     src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAnCAYAAAAVW4iAAAABfElEQVRIS8WXvU6EQBCAZ5YHsdTmEk3kJ1j4HDbGxMbG5N7EwkIaCy18DxtygMFopZ3vAdkxkMMsB8v+XqQi2ex8ux/D7CyC8NR1fdC27RoRszAMv8Ux23ccJhZFcQoA9wCQAMAbEd0mSbKxDTzM6wF5nq+CIHgGgONhgIi+GGPXURTlLhDstDRN8wQA5zOB3hljFy66sCzLOyJaL6zSSRdWVXVIRI9EdCaDuOgavsEJY+wFEY8WdmKlS5ZFMo6xrj9AF3EfukaAbcp61TUBdJCdn85J1yzApy4pwJeuRYAPXUqAqy4tgIsubYCtLiOAjS5jgKkuK8BW1w0APCgOo8wKMHcCzoA+AeDSGKA4AXsOEf1wzq/SNH01AtjUKG2AiZY4jj9GXYWqazDVIsZT7sBGizbAVosWwEWLEuCqZRHgQ4sU4EvLLMCnlgnAt5YRYB9aRoD/7q77kivWFlVZ2R2XdtdiyTUNqpNFxl20bBGT7ppz3t12MhctIuwXEK5/O55iCBQAAAAASUVORK5CYII="/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="division-line"></div>
+
+            <div class="item-row group-list">
+                <div class="item-body" onclick="showGroupProfile('<?php echo($profile["groupId"]) ?>');">
+                    <div class="item-body-display">
+                        <div class="item-body-desc">
+                            <?php echo $profile["name"]; ?>
+                        </div>
+
+                        <div class="item-body-tail">
+                            <div class="item-body-value">
+                                <img class="more-img"
+                                     src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAnCAYAAAAVW4iAAAABfElEQVRIS8WXvU6EQBCAZ5YHsdTmEk3kJ1j4HDbGxMbG5N7EwkIaCy18DxtygMFopZ3vAdkxkMMsB8v+XqQi2ex8ux/D7CyC8NR1fdC27RoRszAMv8Ux23ccJhZFcQoA9wCQAMAbEd0mSbKxDTzM6wF5nq+CIHgGgONhgIi+GGPXURTlLhDstDRN8wQA5zOB3hljFy66sCzLOyJaL6zSSRdWVXVIRI9EdCaDuOgavsEJY+wFEY8WdmKlS5ZFMo6xrj9AF3EfukaAbcp61TUBdJCdn85J1yzApy4pwJeuRYAPXUqAqy4tgIsubYCtLiOAjS5jgKkuK8BW1w0APCgOo8wKMHcCzoA+AeDSGKA4AXsOEf1wzq/SNH01AtjUKG2AiZY4jj9GXYWqazDVIsZT7sBGizbAVosWwEWLEuCqZRHgQ4sU4EvLLMCnlgnAt5YRYB9aRoD/7q77kivWFlVZ2R2XdtdiyTUNqpNFxl20bBGT7ppz3t12MhctIuwXEK5/O55iCBQAAAAASUVORK5CYII="/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="division-line"></div>
+
+        </div>
+    </div>
+
     <div class="layout-all-row">
 
         <div class="list-item-center">
 
             <div class="item-row-title">
-                <div class="" style="flex-direction: row;vertical-align: bottom;width: 100%;">
-                    <div class="item-body-display">
+                <?php if ($lang == "1") { ?>
+                    站点群聊列表
+                <?php } else { ?>
+                    Site Group List
+                <?php } ?>(<?php echo $totalGroupCount ?>)
 
-                        <div class="" style="margin-left: 10px">
-                            <?php if ($lang == "1") { ?>
-                                站点群聊列表
-                            <?php } else { ?>
-                                Site Group List
-                            <?php } ?>
-                        </div>
-
-                    </div>
-                </div>
             </div>
 
             <?php foreach ($groupList as $key => $profile) { ?>
@@ -39,13 +109,19 @@
                     <div class="item-body" onclick="showGroupProfile('<?php echo($profile["groupId"]) ?>');">
                         <div class="item-body-display">
                             <div class="item-body-desc">
-                                <?php echo $profile["name"]; ?>
+                                <?php
+                                $length = mb_strlen($profile['name']);
+                                if ($length > 10) {
+                                    echo mb_substr($profile['name'], 0, 10) . "...";
+                                } else {
+                                    echo $profile['name'];
+                                }
+                                ?>
                             </div>
 
                             <div class="item-body-tail">
                                 <div class="item-body-value">
-                                    <img class="more-img"
-                                         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAnCAYAAAAVW4iAAAABfElEQVRIS8WXvU6EQBCAZ5YHsdTmEk3kJ1j4HDbGxMbG5N7EwkIaCy18DxtygMFopZ3vAdkxkMMsB8v+XqQi2ex8ux/D7CyC8NR1fdC27RoRszAMv8Ux23ccJhZFcQoA9wCQAMAbEd0mSbKxDTzM6wF5nq+CIHgGgONhgIi+GGPXURTlLhDstDRN8wQA5zOB3hljFy66sCzLOyJaL6zSSRdWVXVIRI9EdCaDuOgavsEJY+wFEY8WdmKlS5ZFMo6xrj9AF3EfukaAbcp61TUBdJCdn85J1yzApy4pwJeuRYAPXUqAqy4tgIsubYCtLiOAjS5jgKkuK8BW1w0APCgOo8wKMHcCzoA+AeDSGKA4AXsOEf1wzq/SNH01AtjUKG2AiZY4jj9GXYWqazDVIsZT7sBGizbAVosWwEWLEuCqZRHgQ4sU4EvLLMCnlgnAt5YRYB9aRoD/7q77kivWFlVZ2R2XdtdiyTUNqpNFxl20bBGT7ppz3t12MhctIuwXEK5/O55iCBQAAAAASUVORK5CYII="/>
+                                    <img class="more-img" src="../../public/img/manage/more.png"/>
                                 </div>
                             </div>
                         </div>
@@ -65,10 +141,37 @@
 
 <script type="text/javascript">
 
+    $(".search-input").on('input porpertychange', function () {
+        var val = $(this).val();
+        if (val == "") {
+            $("#search-content").hide();
+        }
+    });
+
+    $(".search-input").on('keypress', function (e) {
+
+        var keycode = e.keyCode;
+        var searchName = $(this).val();
+        if (keycode == '13') {
+            // The Event interface's preventDefault() method tells the user agent that if the event does not get explicitly handled, its default action should not be taken as it normally would be. The event continues to propagate as usual, unless one of its event listeners calls stopPropagation() or stopImmediatePropagation(), either of which terminates propagation at once.
+            e.preventDefault();
+
+            var searchValue = $(this).val();
+            searchGroups(searchValue)
+        }
+    });
+
+    function searchGroups(searchValue) {
+        alert("value=" + searchValue);
+
+        $("#search-content").show();
+    }
+
     function showGroupProfile(groupId) {
         var url = "index.php?action=manage.group.profile&lang=" + getLanguage() + "&groupId=" + groupId;
         zalyjsCommonOpenPage(url);
     }
+
 
 </script>
 
--- src/views/manage/user/indexList.php
+++ src/views/manage/user/indexList.php
@@ -7,34 +7,99 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <link rel="stylesheet" href="../../public/manage/config.css"/>
+    <link rel="stylesheet" href="../../public/manage/search.css"/>
+
+    <style>
+        .item-row-title {
+            /*width: 100%;*/
+            height: 20px;
+            font-size: 14px;
+            font-family: PingFangSC-Medium;
+            font-weight: 500;
+            color: rgba(153, 153, 153, 1);
+            line-height: 20px;
+            margin: 17px 0px 7px 10px;
+        }
+    </style>
+
 </head>
 
 <body>
 
 <div class="wrapper" id="wrapper">
 
+    <div class="item-search">
+        <img class="search-img" width="19px" height="19px" src="../../public/img/manage/search.png">
+        <input class="search-input" placeholder="通过昵称、登录名、手机号搜索成员">
+    </div>
+
+    <div class="layout-all-row" id="search-content" style="display: none">
+        <div class="list-item-center">
+
+            <div id="search-title" class="item-row-title">
+                <?php if ($lang == "1") { ?>
+                    群组搜索结果
+                <?php } else { ?>
+                    Search Groups
+                <?php } ?>
+
+            </div>
+
+            <div class="item-row group-list">
+                <div class="item-body" onclick="showGroupProfile('<?php echo($profile["groupId"]) ?>');">
+                    <div class="item-body-display">
+                        <div class="item-body-desc">
+                            <?php echo $profile["name"]; ?>
+                        </div>
+
+                        <div class="item-body-tail">
+                            <div class="item-body-value">
+                                <img class="more-img"
+                                     src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAnCAYAAAAVW4iAAAABfElEQVRIS8WXvU6EQBCAZ5YHsdTmEk3kJ1j4HDbGxMbG5N7EwkIaCy18DxtygMFopZ3vAdkxkMMsB8v+XqQi2ex8ux/D7CyC8NR1fdC27RoRszAMv8Ux23ccJhZFcQoA9wCQAMAbEd0mSbKxDTzM6wF5nq+CIHgGgONhgIi+GGPXURTlLhDstDRN8wQA5zOB3hljFy66sCzLOyJaL6zSSRdWVXVIRI9EdCaDuOgavsEJY+wFEY8WdmKlS5ZFMo6xrj9AF3EfukaAbcp61TUBdJCdn85J1yzApy4pwJeuRYAPXUqAqy4tgIsubYCtLiOAjS5jgKkuK8BW1w0APCgOo8wKMHcCzoA+AeDSGKA4AXsOEf1wzq/SNH01AtjUKG2AiZY4jj9GXYWqazDVIsZT7sBGizbAVosWwEWLEuCqZRHgQ4sU4EvLLMCnlgnAt5YRYB9aRoD/7q77kivWFlVZ2R2XdtdiyTUNqpNFxl20bBGT7ppz3t12MhctIuwXEK5/O55iCBQAAAAASUVORK5CYII="/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="division-line"></div>
+
+            <div class="item-row group-list">
+                <div class="item-body" onclick="showGroupProfile('<?php echo($profile["groupId"]) ?>');">
+                    <div class="item-body-display">
+                        <div class="item-body-desc">
+                            <?php echo $profile["name"]; ?>
+                        </div>
+
+                        <div class="item-body-tail">
+                            <div class="item-body-value">
+                                <img class="more-img"
+                                     src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAnCAYAAAAVW4iAAAABfElEQVRIS8WXvU6EQBCAZ5YHsdTmEk3kJ1j4HDbGxMbG5N7EwkIaCy18DxtygMFopZ3vAdkxkMMsB8v+XqQi2ex8ux/D7CyC8NR1fdC27RoRszAMv8Ux23ccJhZFcQoA9wCQAMAbEd0mSbKxDTzM6wF5nq+CIHgGgONhgIi+GGPXURTlLhDstDRN8wQA5zOB3hljFy66sCzLOyJaL6zSSRdWVXVIRI9EdCaDuOgavsEJY+wFEY8WdmKlS5ZFMo6xrj9AF3EfukaAbcp61TUBdJCdn85J1yzApy4pwJeuRYAPXUqAqy4tgIsubYCtLiOAjS5jgKkuK8BW1w0APCgOo8wKMHcCzoA+AeDSGKA4AXsOEf1wzq/SNH01AtjUKG2AiZY4jj9GXYWqazDVIsZT7sBGizbAVosWwEWLEuCqZRHgQ4sU4EvLLMCnlgnAt5YRYB9aRoD/7q77kivWFlVZ2R2XdtdiyTUNqpNFxl20bBGT7ppz3t12MhctIuwXEK5/O55iCBQAAAAASUVORK5CYII="/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="division-line"></div>
+
+        </div>
+    </div>
+
     <div class="layout-all-row">
 
         <div class="list-item-center">
 
             <div class="item-row-title">
-                <div class="" style="flex-direction: row;vertical-align: bottom;width: 100%;">
-                    <div class="item-body-display">
-
-                        <div class="" style="margin-left: 10px">
-                            <?php if ($lang == "1") { ?>
-                                站点成员列表
-                            <?php } else { ?>
-                                Site Users
-                            <?php } ?>
-                        </div>
-
-                        <div class="" style="margin-right: 10px">
-                            <?php echo $totalUserCount ?>
-                        </div>
-
-                    </div>
+                <div class="">
+                    <?php if ($lang == "1") { ?>
+                        站点成员列表
+                    <?php } else { ?>
+                        Site Users
+                    <?php } ?>
+                    (<?php echo $totalUserCount ?>)
                 </div>
+
             </div>
 
             <?php foreach ($userList as $key => $profile) { ?>
@@ -42,24 +107,26 @@
                 <div class="item-row">
                     <div class="item-body" onclick="showUserProfile('<?php echo($profile["userId"]) ?>')"
                          id="user-list-id" userId="<?php echo($profile["userId"]) ?>">
-                        <div class="item-body-display">
-                            <div class="item-body-desc">
-                                <?php
+                        <div class="item-body-display" style="align-items: center">
+                            <div class="item-body-desc"><?php
+                                $username = $profile["userId"];
                                 if ($profile["nickname"]) {
-                                    echo $profile["nickname"];
+                                    $username = $profile["nickname"];
                                 } else if ($profile["loginName"]) {
-                                    echo $profile["loginName"];
-                                } else {
-                                    echo $profile["userId"];
+                                    $username = $profile["loginName"];
                                 }
 
-                                ?>
-                            </div>
+                                $length = mb_strlen($username);
+                                if ($length > 16) {
+                                    echo mb_substr($username, 0, 16) . "...";
+                                } else {
+                                    echo $username;
+                                }
+                                ?></div>
 
                             <div class="item-body-tail">
                                 <div class="item-body-value">
-                                    <img class="more-img"
-                                         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAnCAYAAAAVW4iAAAABfElEQVRIS8WXvU6EQBCAZ5YHsdTmEk3kJ1j4HDbGxMbG5N7EwkIaCy18DxtygMFopZ3vAdkxkMMsB8v+XqQi2ex8ux/D7CyC8NR1fdC27RoRszAMv8Ux23ccJhZFcQoA9wCQAMAbEd0mSbKxDTzM6wF5nq+CIHgGgONhgIi+GGPXURTlLhDstDRN8wQA5zOB3hljFy66sCzLOyJaL6zSSRdWVXVIRI9EdCaDuOgavsEJY+wFEY8WdmKlS5ZFMo6xrj9AF3EfukaAbcp61TUBdJCdn85J1yzApy4pwJeuRYAPXUqAqy4tgIsubYCtLiOAjS5jgKkuK8BW1w0APCgOo8wKMHcCzoA+AeDSGKA4AXsOEf1wzq/SNH01AtjUKG2AiZY4jj9GXYWqazDVIsZT7sBGizbAVosWwEWLEuCqZRHgQ4sU4EvLLMCnlgnAt5YRYB9aRoD/7q77kivWFlVZ2R2XdtdiyTUNqpNFxl20bBGT7ppz3t12MhctIuwXEK5/O55iCBQAAAAASUVORK5CYII="/>
+                                    <img class="more-img" src="../../public/img/manage/more.png"/>
                                 </div>
                             </div>
                         </div>
@@ -79,6 +146,31 @@
 <script type="text/javascript" src="../../public/manage/native.js"></script>
 
 <script type="text/javascript">
+
+    $(".search-input").on('input porpertychange', function () {
+        var val = $(this).val();
+        if (val == "") {
+            $("#search-content").hide();
+        }
+    });
+
+    $(".search-input").on('keypress', function (e) {
+
+        var keycode = e.keyCode;
+        var searchName = $(this).val();
+        if (keycode == '13') {
+            // The Event interface's preventDefault() method tells the user agent that if the event does not get explicitly handled, its default action should not be taken as it normally would be. The event continues to propagate as usual, unless one of its event listeners calls stopPropagation() or stopImmediatePropagation(), either of which terminates propagation at once.
+            e.preventDefault();
+
+            var searchValue = $(this).val();
+            searchUsers(searchValue)
+        }
+    });
+
+    function searchUsers(searchValue) {
+        alert(searchValue);
+        $("#search-content").show();
+    }
 
     function showUserProfile(userId) {
         var url = "./index.php?action=manage.user.profile&lang=" + getLanguage() + "&userId=" + userId;
