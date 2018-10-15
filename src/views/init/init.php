306c0007cc69bc6a2240c27a7de681855148e7b5
childeYin <zhangjun516@126.com>
2018-10-15 12:18:03 +0800

    fix upgrade css

--- src/views/init/init.php
+++ src/views/init/init.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>login</title>
+    <title>初始化站点</title>
     <!-- Latest compiled and minified CSS -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="../../public/css/init.css?_version=<?php echo $versionCode?>">
