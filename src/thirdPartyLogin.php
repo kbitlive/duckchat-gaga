08e1c8bba7577051f12963fb421374229a1853b1
SAM2O2O <an.guoyue254@gmail.com>
2018-11-12 12:35:15 +0800

    remove thirdPartyLogin config

--- src/thirdPartyLogin.php
+++ src/thirdPartyLogin.php
@@ -1,10 +1,16 @@
 <?php
 return array(
-    'discuz' =>
-        array(
-            'name' => 'discuz',
-            'logo' => '',
-            'landingUrl' => "http://192.168.3.152:8034/member.php?mod=logging&action=login",
-            'verifyUrl' => "http://192.168.3.152:8034/plugin.php?id=duckchat&action=api.session.verify&body_format=base64pb",
-        ),
+//    'demo' => array(
+//        'name' => 'DemoName',
+//        'logo' => 'https://demo.XXX.com/demo.png',
+//        'landingUrl' => "http://demo.xxx.com/index.php?action=page.site.login",
+//        'verifyUrl' => "http://demo.xxx.com/index.php?action=api.session.verify&body_format=base64pb",
+//    ),
+//    'discuz' =>
+//        array(
+//            'name' => 'discuz',
+//            'logo' => '',
+//            'landingUrl' => "http://demo.xxx.com/member.php?mod=logging&action=login",
+//            'verifyUrl' => "http://demo.xxx.com/plugin.php?id=duckchat&action=api.session.verify&body_format=base64pb",
+//        ),
 );
