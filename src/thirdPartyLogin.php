75b3a7b56c753533fafd4909c394c38678c185c6
childeYin <zhangjun516@126.com>
2018-11-12 12:40:07 +0800

    fix

--- src/thirdPartyLogin.php
+++ src/thirdPartyLogin.php
@@ -1,10 +1,4 @@
 <?php
 return array(
-    'discuz' =>
-        array(
-            'name' => 'discuz',
-            'logo' => '',
-            'landingUrl' => "http://192.168.3.152:8034/member.php?mod=logging&action=login",
-            'verifyUrl' => "http://192.168.3.152:8034/plugin.php?id=duckchat&action=api.session.verify&body_format=base64pb",
-        ),
+
 );
