430bb4d6436e21a912ee8de810d11bec624464d1
childeYin <zhangjun516@126.com>
2018-11-13 20:26:35 +0800

    fix

--- src/controller/MiniProgram/Search/recommend.php
+++ src/controller/MiniProgram/Search/recommend.php
@@ -15,6 +15,6 @@ return array(
     ),
     'miniProgramId'    => "200",
     "about_us_desc"    => "欢迎加入核武交流协作大家庭，！本平台致力于打造最安全的警务工作交流协作，服务器专门搭建，内容私密，阅后即焚，安全无过滤，请大家珍惜使用！平台初建，功能不断完善中……",
-    "about_us_title"   => "核武站技术团队",
-    "about_us_contact" => "李萌：13273247332"
+    "about_us_title"   => "警界核武器创研小组",
+    "about_us_contact" => "微信号：zwtand911"
 );
