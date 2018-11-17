939b51c9e0e07bef58512c1efbe939dab78ba9e3
SAM2O2O <an.guoyue254@gmail.com>
2018-11-17 17:58:37 +0800

    fix bug

--- src/controller/Manage/Custom/Manage_Custom_RegisterController.php
+++ src/controller/Manage/Custom/Manage_Custom_RegisterController.php
@@ -6,84 +6,60 @@
  * Time: 11:49 AM
  */
 
-class Manage_Custom_RegisterController extends Manage_CommonController
+class Manage_Custom_RegisterController extends Manage_ServletController
 {
 
-    /**
-     * 处理正式的请求逻辑，比如跳转界面，post获取信息等
-     */
-    protected function doRequest()
+    protected function doGet()
     {
+        $loginConfig = $this->ctx->Site_Custom->getLoginAllConfig();
 
-        $method = $_SERVER["REQUEST_METHOD"];
+        $loginNameAliasConfig = isset($loginConfig[LoginConfig::LOGIN_NAME_ALIAS]) ? $loginConfig[LoginConfig::LOGIN_NAME_ALIAS] : "";
+        $loginNameAlias = isset($loginNameAliasConfig["configValue"]) ? $loginNameAliasConfig["configValue"] : "";
 
-        if ($method == "POST") {
-            try {
-                $key = $_POST["key"];
-                $value = $_POST["value"];
+        $userCustomsForRegister = $this->getUserCustomForRegister();
 
-                if ($key == LoginConfig::LOGIN_PAGE_BACKGROUND_IMAGE) {
-                    $fileId = $value;
-                    $imageDir = WPF_LIB_DIR . "../public/site/image/";
-                    $this->ctx->File_Manager->moveImage($fileId, $imageDir);
-                }
+        $params = [
+            "lang" => $this->language,
+            "loginNameAlias" => $loginNameAlias,
+            "userCustoms" => $userCustomsForRegister,
+        ];
 
-                $res = $this->ctx->Site_Custom->updateLoginConfig($key, $value, "", $this->userId);
-                if ($res) {
-                    $result["errCode"] = "success";
-                }
-            } catch (Exception $e) {
-                $result["errInfo"] = $e->getMessage();
-                $this->logger->error($this->action, $e);
-
-            }
-            echo json_encode($result);
-        } else {
-            //1.siteConfig
-            $config = $this->ctx->Site_Config->getAllConfig();
-            $loginMiniProgramId = $config[SiteConfig::SITE_LOGIN_PLUGIN_ID];
-            $enableInvitationCode = $config[SiteConfig::SITE_ENABLE_INVITATION_CODE];
-
-            //2.loginConfig
-            $loginConfig = $this->ctx->Site_Custom->getLoginAllConfig();
-
-            $loginNameAliasConfig = isset($loginConfig[LoginConfig::LOGIN_NAME_ALIAS]) ? $loginConfig[LoginConfig::LOGIN_NAME_ALIAS] : "";
-            $loginNameAlias       = isset($loginNameAliasConfig["configValue"]) ? $loginNameAliasConfig["configValue"] : "";
-            $passwordResetConfig  = isset($loginConfig[LoginConfig::PASSWORD_RESET_WAY]) ? $loginConfig[LoginConfig::PASSWORD_RESET_WAY] : "";
-            $passwordResetWay     = isset($passwordResetConfig["configValue"]) ? $passwordResetConfig["configValue"] : "";
-
-            $passwordResetRequiredConfig = isset( $loginConfig[LoginConfig::PASSWORD_RESET_REQUIRED]) ?  $loginConfig[LoginConfig::PASSWORD_RESET_REQUIRED] : "";
-            $passwordResetRequired = isset( $passwordResetRequiredConfig["configValue"]) ?  $passwordResetRequiredConfig["configValue"] : "";
-
-            //3.miniProgramName
-            $miniProgramName = $this->getMiniProgramName($loginMiniProgramId);
-
-            $params = [
-                "lang"                  => $this->language,
-                "loginPluginId"         => $loginMiniProgramId,
-                "loginNameAlias"        => $loginNameAlias,
-                "passwordResetWay"      => $passwordResetWay,
-                "passwordResetRequired" => $passwordResetRequired,
-                "miniProgramName"       => $miniProgramName . "后台",
-                "enableInvitationCode"  => $enableInvitationCode,
-            ];
-
-            echo $this->display("manage_custom_register", $params);
-
-        }
-
+        echo $this->display("manage_custom_register", $params);
         return;
     }
 
-    private function getMiniProgramName($miniProgramId)
+    protected function doPost()
     {
-        $profile = $this->getMiniProgramProfile($miniProgramId);
-        return $profile["name"];
+        $result = [
+            "errCode" => "error",
+        ];
+
+        try {
+            $key = $_POST["key"];
+            $value = $_POST["value"];
+
+            if ($key == LoginConfig::LOGIN_PAGE_BACKGROUND_IMAGE) {
+                $fileId = $value;
+                $imageDir = WPF_LIB_DIR . "../public/site/image/";
+                $this->ctx->File_Manager->moveImage($fileId, $imageDir);
+            }
+
+            $res = $this->ctx->Site_Custom->updateLoginConfig($key, $value, "", $this->userId);
+            if ($res) {
+                $result["errCode"] = "success";
+            }
+        } catch (Exception $e) {
+            $result["errInfo"] = $e->getMessage();
+            $this->logger->error($this->action, $e);
+
+        }
+        echo json_encode($result);
+        return;
     }
 
-    private function getMiniProgramProfile($miniProgramId)
+    private function getUserCustomForRegister()
     {
-        return $this->ctx->SitePluginTable->getPluginById($miniProgramId);
+        return $this->ctx->SiteUserCustomTable->getColumnInfosForRegister();
     }
 }
 
