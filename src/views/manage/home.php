4aab3bff7f2aaecdb411a2974a536dcd108587be
childeYin <zhangjun516@126.com>
2018-11-26 17:04:44 +0800

    fix

--- src/views/manage/home.php
+++ src/views/manage/home.php
@@ -464,8 +464,25 @@
 
 
     function showPluginAdmin(url) {
-        url += "&lang=" + getLanguage();
+        var duckchatSessionId = getCookie("duckchat_sessionid");
+        url += "&duckchat_sessionid="+duckchatSessionId+"&lang=" + getLanguage();
         openPage(url);
+    }
+
+    function getCookie(name) {
+        try{
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return false;
+        }catch(error) {
+
+        }
+        return false;
     }
 
 </script>
